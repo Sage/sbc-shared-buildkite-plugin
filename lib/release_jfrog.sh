d03c874ef5263fccc6fa586439fa1938fa1396b4
Rolf Wadstein <rolf.wadstein@sage.com>
2022-07-27 11:52:40 -0400

    support for publishing gem and using default gh branch name

---
+++ lib/release_jfrog.sh
@@ -0,0 +1,20 @@
+
+publish_gem() {
+  bundle exec rake build $APP.gemspec
+
+  local RUBYGEMS_HOST=${GEM_HOST:-"https://sageonegems.jfrog.io/sageonegems/api/gems/gems-local"}
+
+  echo "Gems Host: $RUBYGEMS_HOST"
+
+  mkdir -p ~/.gem
+  curl -u $ART_USER:$ART_PASS $RUBYGEMS_HOST/api/v1/api_key.yaml > ~/.gem/credentials
+  chmod 600 ~/.gem/credentials
+
+  local GEMS_PATH=${GEM_PATH:-"pkg/*.gem"}
+
+  echo "Gem Path: $GEMS_PATH"
+
+  gem push /usr/src/app/$GEMS_PATH
+
+  echo "Push Complete"
+}
