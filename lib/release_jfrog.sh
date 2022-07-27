1cefedb718707f56ffe9cffde8569c915b370cb9
Rolf Wadstein <rolf.wadstein@sage.com>
2022-07-27 15:15:26 -0400

    removed local keyword since no longer function

--- lib/release_jfrog.sh
+++ lib/release_jfrog.sh
@@ -4,7 +4,7 @@ set -e
 
 bundle exec rake build $APP.gemspec
 
-local RUBYGEMS_HOST=${GEM_HOST:-"https://sageonegems.jfrog.io/sageonegems/api/gems/gems-local"}
+RUBYGEMS_HOST=${GEM_HOST:-"https://sageonegems.jfrog.io/sageonegems/api/gems/gems-local"}
 
 echo "Gems Host: $RUBYGEMS_HOST"
 
@@ -12,7 +12,7 @@ mkdir -p ~/.gem
 curl -u $ART_USER:$ART_PASS $RUBYGEMS_HOST/api/v1/api_key.yaml > ~/.gem/credentials
 chmod 600 ~/.gem/credentials
 
-local GEMS_PATH=${GEM_PATH:-"pkg/*.gem"}
+GEMS_PATH=${GEM_PATH:-"pkg/*.gem"}
 
 echo "Gem Path: $GEMS_PATH"
 
